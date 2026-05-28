177c1778edeb3718eb53207fff7e5e822b2a5bb6
Darshan1355 <darshanamane083@gmail.com>
2026-05-28 12:28:15 +0530

    database deployed

--- backend/app.py
+++ backend/app.py
@@ -17,5 +17,31 @@ app.register_blueprint(complaint_bp)
 app.register_blueprint(guide_bp)
 app.register_blueprint(equipment_bp)
 
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+DB_USER = os.environ.get("MYSQLUSER")
+DB_PASSWORD = os.environ.get("MYSQLPASSWORD")
+DB_HOST = os.environ.get("MYSQLHOST")
+DB_PORT = os.environ.get("MYSQLPORT")
+DB_NAME = os.environ.get("MYSQLDATABASE")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+@app.route("/")
+def home():
+    return "Backend Running"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
